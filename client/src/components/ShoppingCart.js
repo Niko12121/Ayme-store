e38c0c531d82868c7ee5b088197266c6ff4acfe2
Niko12121 <vdonat@uc.cl>
2021-10-18 15:52:44 -0300

    Shopping Cart actuallize fast

--- client/src/components/ShoppingCart.js
+++ client/src/components/ShoppingCart.js
@@ -9,41 +9,50 @@ export default function SearchingCart(props) {
     useEffect(() => {
         Axios.get("http://localhost:3001/shoppingCart", {
             params: {idUser: idUser}}).then((pa) => {
-                console.log(pa.data)
                 setCart(pa.data)
         })
     }, [idUser])
 
+    return <div className='shoppingCart'>
+            <h4>Carrito de Compra</h4>
+                {cart.map(product => {
+                    return <ProductShopping product={product} idUser={idUser} />
+                })}<br/>
+                <button> Ir a Pagar </button>
+        </div>
+}
+
+function ProductShopping(props) {
+    const [quantity, setQuantity] = useState(props.product.quantity)
+    const [show, setShow] = useState(true)
+
     const updateQuantity = (idProduct, newQuantity) => {
         if (newQuantity === 0) {
             Axios.delete("http://localhost:3001/shoppingDelete", {
-            data: {idUser: idUser, idProduct: idProduct}
-        })} else {
+            data: {idUser: props.idUser, idProduct: idProduct}
+            
+        })
+            setShow(false)
+        } else {
             Axios.put(`http://localhost:3001/shoppingUpdate`, {
-                idUser: idUser,
+                idUser: props.idUser,
                 idProduct: idProduct,
                 quantity: newQuantity
             });
         }
         /* Change this */
-        window.location.reload()
+        setQuantity(newQuantity)
     }
 
-    return <div className='shoppingCart'>
-            <h4>Carrito de Compra</h4>
-                {cart.map(product => {
-                    return <div className="productShopping">
-                        {product.name} ${product.actual_value} c/u<br/>
-                        <a href={`/products/${product.idProduct}`}>
-                        <img className="imgShopping" alt="No cargo" src={require('../Images/' + product.file.toString() + '.jpg').default}></img>
-                        </a><br />
-                        <button onClick={() => updateQuantity(product.idProduct, 0)}> E </button>
-                        <button onClick={() => updateQuantity(product.idProduct, product.quantity - 1)}> - </button>
-                        {product.quantity}
-                        <button onClick={() => updateQuantity(product.idProduct, product.quantity + 1)}> + </button>
-                        <b> ${product.quantity * product.actual_value}</b>
-                    </div>
-                })}<br/>
-                <button> Ir a Pagar </button>
-        </div>
+    return show && <div className="productShopping">
+        {props.product.name} ${props.product.actual_value} c/u<br/>
+        <a href={`/products/${props.product.idProduct}`}>
+        <img className="imgShopping" alt="No cargo" src={require('../Images/' + props.product.file.toString() + '.jpg').default}></img>
+        </a><br />
+        <button onClick={() => updateQuantity(props.product.idProduct, 0)}> E </button>
+        <button onClick={() => updateQuantity(props.product.idProduct, quantity - 1)}> - </button>
+        {quantity}
+        <button onClick={() => updateQuantity(props.product.idProduct, quantity + 1)}> + </button>
+        <b> ${props.product.quantity * props.product.actual_value}</b>
+    </div>
 }
